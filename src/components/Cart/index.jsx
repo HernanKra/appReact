73dfc656c6325f21a23eb14a26ba5fe45635e4bd
Hernan Krauchuk <hernankrauchuk@gmail.com>
2022-09-27 22:52:26 -0300

    generando orden de compra

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,9 +2,27 @@ import { useCartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom";
 import ItemCart from '../ItemCart';
 import './cart.css';
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
 
 const Cart = () => {
     const { cart, totalPrice, clearCart } = useCartContext();
+    
+    const order = {
+        buyer : {
+            name: 'Prueba',
+            email: 'prueba@gmail.com',
+            phone: '1234567'
+        },
+        items: cart.map(product => ({ id: product.id, nombre: product.name, price: product.price, quantity: product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(({ id }) => console.log(id), alert('Su orden de compra fue generada exitosamente.'))
+    }
 
     if (cart.length === 0) {
         return (
@@ -22,6 +40,7 @@ const Cart = () => {
             <div className="vaciarCarro">
             <p className="pTotal">Total : $ {totalPrice()}</p>
             <button onClick={() => clearCart()}> Vaciar Carrito </button>
+            <button onClick={handleClick}>Generar Orden de Compra</button>
             </div>
         </>
     )
